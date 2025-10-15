1a7f0ab3606ba5647766be88ebc1aed26b240742
Eleonora Alminson <ellialminson@me.com>
2025-10-15 21:39:37 +0200

    Changed from static class DeviceState.cs to public interface/class IDeviceStateProvider/DeviceStateProvider

--- MainApp/Services/DeviceState.cs
+++
@@ -1,9 +0,0 @@
-﻿using MainApp.Models;
-
-namespace MainApp.Services;
-
-public static class DeviceState
-{
-    public static Func<object>? GetStatusFunc { get; set; }
-    public static Action<CommandRequest>? HandleCommandAction { get; set; }
-}
---
+++ MainApp/Services/DeviceStateProvider.cs
@@ -0,0 +1,23 @@
+﻿using MainApp.Models;
+
+namespace MainApp.Services;
+
+public class DeviceStateProvider : IDeviceStateProvider
+{
+    private readonly MainWindow _mainWindow;
+
+    public DeviceStateProvider(MainWindow mainWindow)
+    {
+        _mainWindow = mainWindow ?? throw new ArgumentNullException(nameof(mainWindow));
+    }
+
+    public object GetStatus()
+    {
+        return _mainWindow.GetDeviceStatus();
+    }
+
+    public void HandleCommand(CommandRequest command)
+    {
+        _mainWindow.HandleRestCommand(command);
+    }
+}
